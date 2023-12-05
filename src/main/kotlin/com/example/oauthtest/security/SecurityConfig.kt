5f642e974bddbc8acbe22a4c80816fd1da85a15b
slolee <slolee@naver.com>
2023-12-06 01:25:16 +0900

    Feature: Cors 이슈 해결

--- src/main/kotlin/com/example/oauthtest/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/oauthtest/security/SecurityConfig.kt
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class SecurityConfig(
@@ -19,19 +22,31 @@ class SecurityConfig(
         return http.formLogin { it.disable() }
             .httpBasic { it.disable() }
             .csrf { it.disable() }
-            .cors { it.disable() }
+            .cors { it.configurationSource(corsConfigurationSource()) }
             .sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
             .oauth2Login { oauthConfig ->
                 oauthConfig.redirectionEndpoint {
                     it.baseUri("/oauth2/*/callback")
-                }
-                oauthConfig.userInfoEndpoint {
+                }.userInfoEndpoint {
                     it.userService(oauthUserService)
-                }
-                oauthConfig.successHandler(oAuthLoginSuccessHandler)
+                }.successHandler(oAuthLoginSuccessHandler)
             }
             .build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val corsConfig = CorsConfiguration()
+
+        corsConfig.addAllowedOriginPattern("*")
+        corsConfig.addAllowedHeader("*")
+        corsConfig.addAllowedMethod("*")
+        corsConfig.allowCredentials = true
+
+        val urlSource = UrlBasedCorsConfigurationSource()
+        urlSource.registerCorsConfiguration("/**", corsConfig)
+        return urlSource
     }
 }
